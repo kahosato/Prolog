4f182f321c53972f2c2c196b2458314b8e754982
Kaho Sato <KahoSato@dyn1207-233.wlan.ic.ac.uk>
2013-11-28 12:09:17 +0000

    Add ex1

---
+++ ex1/family.pl
@@ -0,0 +1,54 @@
+/*
+
+    MAC, MCSs, MRes - 
+    Module 531: Laboratory (Prolog)
+
+    Comp 2: 276 Introduction to Prolog
+
+    File: family.pl - cjh
+
+*/
+
+child_of( emmeline, frank ). 
+child_of( amelia, frank ).
+child_of( harold, frank ).
+child_of( chris, amelia ).
+child_of( chris, john ).
+child_of( emlyn, chris ).
+child_of( emlyn, elizabeth ).
+child_of( brendon, chris ).
+child_of( brendon, elizabeth ). 
+child_of( irene, maurice ).
+child_of( les, maurice ).
+child_of( elizabeth, irene ).
+child_of( elizabeth, george ).
+child_of( margaret, irene ).
+child_of( margaret, george ).
+child_of( rebecca, margaret ).
+child_of( louise, margaret ).   
+child_of( nick, margaret ).
+child_of( rebecca, peter ).
+child_of( louise, peter ).
+child_of( nick, peter ).
+
+male( frank ).   
+male( harold ).
+male( chris ).
+male( john ).
+male( emlyn ).
+male( brendon ).
+male( maurice ).
+male( les ).
+male( nick ).
+male( peter ).
+male( george ).
+
+female( emmeline ).
+female( amelia ).
+female( elizabeth ).
+female( irene ).
+female( margaret ).
+female( rebecca ).
+female( louise ).
+
+% end of data
