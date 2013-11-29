3f3f1e8c96200d18ba6ad3afe0e80f4ced0341e6
Kaho Sato <ks3512@vm-shell3.doc.ic.ac.uk>
2013-11-29 11:56:52 +0000

    Complete up to step 3

--- ex4/crossings.pl
+++ ex4/crossings.pl
@@ -45,8 +45,35 @@ solution([f(g),f,f(b),f,f(c),f(g),f(w),f,f(g)]).
 
 %% ------ Add your code to this file here.
 
+%% ------ step 1.
+in_farm(X) :- member(X, [f, w, g, c, b]).
+all_in_farm([]).
+all_in_farm([X|L]):- in_farm(X), all_in_farm(L).
+safe([f|X]) :- all_in_farm(X).
+safe([w, c]).
+safe([w, b]).
+safe([w, c, b]).
+safe([g, b]).
+safe([X]) :- in_farm(X).
+%% ------ step 2.
+safe_state(N-S) :- safe(N), safe(S).
+%% ------ step 3.
+include([], L2).
+include([X|L], L2) :- member(X, L2), include(L, L2).
+same_bank(B1, B2) :-
+    all_in_farm(B1),
+    all_in_farm(B2),
+    length(B1, X), 
+    length(B2, X), 
+    include(B1, B2).
+equiv(N1-S1, N2-S2) :-
+    same_bank(N1, N2),
+    same_bank(S1, S2).
 
+
+%% ------ step 4.
 /*  Remove the following comments if you wish to skip Step 6 */
+safe_state(North-South) :- safe(North), safe(South).
 
 /* 
 
