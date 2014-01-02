9f3d8f653f763f79cbfab989ec20a4fd349c89f6
Kaho Sato <kahosato93@gmail.com>
2014-01-02 05:58:04 +0000

    least

--- ex2/two.pl
+++ ex2/two.pl
@@ -6,10 +6,13 @@ odd(s(0)).
 odd(s(s(X))):- odd(X).
 
 ones_zeros([]).
-ones_zeros([1|X]) :- ones_zeros(X),!.
-ones_zeros([0|X]) :- ones_zeros(X),!.
+ones_zeros([1|X]) :- ones_zeros(X).
+ones_zeros([0|X]) :- ones_zeros(X).
 
 hasdups([X|Y]) :- member(X, Y).
+
+least(X, Y, X) :- X < Y, !.
+least(X, Y, Y) :- X >= Y, !.
 
 
 
