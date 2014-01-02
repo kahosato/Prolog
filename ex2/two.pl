01eeb6b45efd7724aae05bcbde031206eb2e9ca3
Kaho Sato <kahosato93@gmail.com>
2014-01-02 07:17:03 +0000

    containS?.

--- ex2/two.pl
+++ ex2/two.pl
@@ -10,9 +10,23 @@ ones_zeros([1|X]) :- ones_zeros(X).
 ones_zeros([0|X]) :- ones_zeros(X).
 
 hasdups([X|Y]) :- member(X, Y).
+hasdups([X|Y]) :- hasdups(Y).
 
 least(X, Y, X) :- X < Y, !.
 least(X, Y, Y) :- X >= Y, !.
 
+prod(L, P):- prodh(L, A, P).
+prodh([], A, A).
+prodh([X|Y], A, P):-
+    Acc is A * Y,
+    prodh(Y, Acc, P).
 
-
+contains(L1, L2, N):-
+    length(L1, X),
+    length(L2, Y),
+    M is N-1,
+    L is X-Y-N+1
+    length(F, M),
+    length(A, L),
+    append(F, L2, S),
+    append(S, A, L1).
