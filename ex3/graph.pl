b17926901575a8033f9503f0235b90852e0744a9
Kaho Sato <kahosato93@gmail.com>
2014-01-08 12:20:28 +0000

    Typo connected -> connected_n

--- ex3/graph.pl
+++ ex3/graph.pl
@@ -9,12 +9,17 @@ edge(p, m).
 edge(m, n).
 edge(n, t).
 edge(b, g).
-connected_parts(Connected) :- findall([X], node(X), Xs), c_p(Xs, Connected).
+
+connected_parts(Connected) :-
+    findall([X], node(X), Xs), 
+    c_p(Xs, Connected).
+
 c_p(Connected, Connected) :-
     \+(member(C1, Connected),
     member(C2, Connected),
     C1 \= C2,
-    connected_c(C1, C2)).    
+    connected_c(C1, C2)).
+
 c_p(So_far, Connected) :-
     member(C1, So_far),
     member(C2, So_far),
@@ -27,14 +32,24 @@ c_p(So_far, Connected) :-
 
 % function that removes element S on its first occurence.
 rm(S, L1, L2) :- rm(S, L1, [], L2).
-rm(S, [], L2, L2).
+rm(_, [], L2, L2).
 rm(S, [S|T1], A, L2) :- append(A, T1, L2).
-rm(S, [H|T1], A, L2]) :- rm(S, T1, [H|A], L2).
+rm(S, [H|T1], A, L2) :- rm(S, T1, [H|A], L2).
 
-connected_n(E, [H|T]) :- edge(E, H) ; edge(H, E) ; connected(E, T).
-connected_c([X], Y) :- connected(X, Y).
-connected_c([H|T], Y) :- connected(H, Y) ; connected_c(T, Y).
+% First argument: node, second argument: component.
+connected_n(N, [H|T]) :- 
+    edge(N, H) ;
+    edge(H, N) ;
+    connected_n(N, T).
+
+% first argument: component, second argument: component.
+connected_c([X], Y) :-
+    connected_n(X, Y).
+connected_c([H|T], Y) :-
+    connected_n(H, Y) ;
+    connected_c(T, Y).
+
 merge(X, [], X).
 merge([], Y, Y).
 merge([H|T], [H2|T2], [H|T3]) :- H =< H2,!, merge(T, [H2|T2], T3).
-merge([H|T], [H2|T2], [H|T3]) :- merge([H|T], T2, T3).
+merge([H|T], [H2|T2], [H2|T3]) :- merge([H|T], T2, T3).
