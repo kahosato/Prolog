1a8a4845d67d1570825b27c617546d1f3ef4e393
Kaho Sato <ks3512@vm-shell2.doc.ic.ac.uk>
2014-01-12 16:12:30 +0000

    Done with nqueen

--- ex5/lazy.pl
+++ ex5/lazy.pl
@@ -31,3 +31,27 @@ q8_h([Q|T], Qs, I) :-
   NewI is I + 1,
   append(Qs, [Q], NQs),
   q8_h(T, NQs, NewI).
+
+in_range(Min, Max, Min) :-
+  Min =< Max.
+
+in_range(Min, Max, X) :-
+  Min =< Max,
+  NewM is Min + 1,
+  in_range(NewM, Max, X).
+
+on_board_n(H, N) :-
+  in_range(1, N, H).
+
+no_attack_n(P, V, H, N) :- on_board_n(H, N), n_a_h(P, 1, V, H).
+
+nqueen(Sol, N) :- nq_h(Sol, [], 1, N).
+nq_h([], _, I, N) :-
+  I is N + 1.
+nq_h([Q|T], Qs, I, N) :-
+  no_attack_n(Qs, I, Q, N),
+  NewI is I+1,
+  append(Qs, [Q], NQs),
+  nq_h(T, NQs, NewI, N).
+
+
